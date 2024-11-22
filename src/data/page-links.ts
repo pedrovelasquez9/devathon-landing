3fea2e6809421bb536d9b5f14a04d55673dee9c6
Joselu <joseluissgz97@gmail.com>
2024-11-22 13:06:08 +0100

    feat: add devathon VIII page

--- src/data/page-links.ts
+++ src/data/page-links.ts
@@ -1,18 +1,20 @@
 import type { LinkItem } from "../interfaces/Links.interface";
 
 export const links: LinkItem[] = [
-  {
-    name: "Inicio",
-    href: "/",
-  },
-  {
-    name: "Newsletter",
-    href: "/newsletter",
-  },
-  { name: "Redes", href: "/redes" },
-  { name: "Recursos", href: "/recursos" },
-  { name: "Devathon", href: "/devathon" },
-  { name: "VII edición del Devathon", href: "/devathon-vii-edition" },
-  { name: "Retos de programación", href: "/retos" },
-  { name: "Jugar a Órbita", href: "/orbita" }
+	{
+		name: "Inicio",
+		href: "/",
+	},
+	{
+		name: "Newsletter",
+		href: "/newsletter",
+	},
+	{ name: "Redes", href: "/redes" },
+	{ name: "Recursos", href: "/recursos" },
+	{ name: "Devathon", href: "/devathon" },
+	{ name: "VII edición del Devathon", href: "/devathon-vii-edition" },
+	{ name: "VIII edición del Devathon", href: "/devathon-viii-edition" },
+
+	{ name: "Retos de programación", href: "/retos" },
+	{ name: "Jugar a Órbita", href: "/orbita" },
 ];
