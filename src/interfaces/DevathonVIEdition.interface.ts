20ccb90b743ab4564f345f63d927994e507decaf
Joselu <joseluissgz97@gmail.com>
2025-03-31 22:16:37 +0200

    feat: update interface for devathon IX

--- src/interfaces/DevathonVIEdition.interface.ts
+++ src/interfaces/DevathonVIEdition.interface.ts
@@ -1,42 +1,43 @@
 export interface Team {
-    id: string;
-    name: string;
-    isWinner: boolean;
-    isRetired: boolean;
-    project?: string;
-    frontendRepo?: string;
-    backendRepo?: string;
-    members?: TeamMember[];
+  id: string;
+  name: string;
+  team?: string;
+  isWinner: boolean;
+  isRetired: boolean;
+  project?: string;
+  frontendRepo?: string;
+  backendRepo?: string;
+  members?: TeamMember[];
 }
 
 export interface TeamMember {
-    name: string;
-    img: string;
-    linkedin: string;
+  name: string;
+  img: string;
+  linkedin: string;
 }
 
 export interface DevathonMenthor {
-    name: string;
-    img: string;
-    linkedin: string;
+  name: string;
+  img: string;
+  linkedin: string;
 }
 
 export interface Network {
-    name: string;
-    link: string;
-    icon: string;
+  name: string;
+  link: string;
+  icon: string;
 }
 
 export interface Judge {
-    name: string;
-    project: string;
-    networks: Network[];
-    img: string;
+  name: string;
+  project: string;
+  networks: Network[];
+  img: string;
 }
 
 export interface DevathonVIEditionData {
-    devathon_edition: number;
-    teams: Team[];
-    judges: Judge[];
-    mehtors: DevathonMenthor[];
+  devathon_edition: number;
+  teams: Team[];
+  judges: Judge[];
+  mehtors: DevathonMenthor[];
 }
